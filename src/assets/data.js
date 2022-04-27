b55ac4ba4490ec8ba648f432613461f2d0f5bc9c
unuefeejovwoke <vuwill7114@gmail.com>
2022-04-26 20:31:50 -0700

    created files and url routes

---
+++ src/assets/data.js
@@ -0,0 +1,21 @@
+let notes = [
+    {
+        "id": 1,
+        "body": "workout in the morning",
+        "updated": "2021-07-14T13:49:02.078653Z",
+        
+    },
+    {
+        "id": 2,
+        "body": "Continue avedok website",
+        "updated": "2021-07-13T20:43:18.550058Z",
+    },
+    {
+        "id": 3,
+        "body": "react 30 days challenge",
+        "updated": "2021-07-13T19:46:12.187306Z",
+    }
+]
+
+
+export default notes;
